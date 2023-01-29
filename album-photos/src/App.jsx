3c72af011957bad1a583953e6e7595cbef9c8af5
phinic925 <phinicnyangate480@gmail.com>
2023-01-29 16:05:54 +0300

    displays all albums

--- album-photos/src/App.jsx
+++ album-photos/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Album from "./components/Album";
 import Register from "./components/auth/Register";
 // import Home from "./components/Home";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Register />
+      <Album/>
       {/* <Home/> */}
     </div>
   );
