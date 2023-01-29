4198719fd913fb240eeabb40aca39ca09d1acd25
phinic925 <phinicnyangate480@gmail.com>
2023-01-29 19:31:27 +0300

    creates a table for displaying users

--- album-photos/src/App.jsx
+++ album-photos/src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 // import Album from "./components/Album";
 // import Register from "./components/auth/Register";
 import Home from "./components/Home";
+import User from "./components/users";
 
 function App() {
   return (
     <div className="App">
       {/* <Register />
       <Album/> */}
-      <Home/>
+      {/* <Home/> */}
+      <User/>
     </div>
   );
 }
--- album-photos/src/components/Home.jsx
+++ album-photos/src/components/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import {
+  MDBCardImage
+  } from "mdb-react-ui-kit";
 function Home() {
     return(
         <div>
-            
+            {/* <MDBCardImage
+                src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg"
+                alt="login form"
+                className="rounded-start w-100"
+              />  */}
         </div>
     )
 }
