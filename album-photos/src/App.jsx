a3de48c36a92f06fe310de8008e3ce5695ba298d
phinic925 <phinicnyangate480@gmail.com>
2023-01-29 16:29:58 +0300

    creates

--- album-photos/src/App.jsx
+++ album-photos/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import Album from "./components/Album";
-import Register from "./components/auth/Register";
-// import Home from "./components/Home";
+// import Album from "./components/Album";
+// import Register from "./components/auth/Register";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <Register />
-      <Album/>
-      {/* <Home/> */}
+      {/* <Register />
+      <Album/> */}
+      <Home/>
     </div>
   );
 }
